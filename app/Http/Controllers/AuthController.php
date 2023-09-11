640fa891207acf3dc4ca41dd54ec448b46c542e9
FelipeSoft <felipedecastrolima@gmail.com>
2023-09-11 09:56:09 -0300

    refactor: Otimizando verificações de token válido, status da autenticação e revogar token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\JWTFactory;
 
 class AuthController extends Controller
 {
@@ -17,10 +18,9 @@ class AuthController extends Controller
 
         $credentials = $request->only("email", "password");
         if($token = JWTAuth::attempt($credentials)){
-            $cookie = Cookie::make("token", $token, 60);
+            $cookie = Cookie::make("token", $token, 1);
 
-            return response()
-            ->json([
+            return response()->json([
                 'message' => 'Authentication has been successful.'
             ], 201)
             ->withCookie($cookie);
@@ -31,14 +31,50 @@ class AuthController extends Controller
         ], 401);
     }
 
-    public function check(Request $request) {
+    public function checkToken(Request $request) {
         if($request->cookie("token")){
-            return response()->json([
-                "message" => "Authenticated"
-            ], 201);
-        }
+            $token = $request->cookie("token");
+            
+            try {
+                JWTAuth::setToken($token);
+                $user = JWTAuth::parseToken()->authenticate();
+    
+                if($user) {
+                    return response()->json([
+                        "message" => "Authenticated",
+                        "logged_user" => [
+                            "id" => $user["id"],
+                            "name" => $user["name"],
+                            "last_name" => $user["last_name"],
+                            "email" => $user["email"],
+                            "telephone" => $user["telephone"]
+                        ]
+                    ], 200);
+                } 
+            } catch (\Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+                return response()->json([
+                    "message" => "Token expired"
+                ], 401);
+            } catch (\Exception $error) {
+                return response()->json([
+                    "message" => "Error on generating process of a Json Web Token (JWT)"
+                ], 401);
+            }
+        }       
         return response()->json([
             "message" => "Not authenticated"
-        ], 200);
+        ], 401);
+    }
+
+    public function destroyToken(Request $request){
+        if($request->cookie("token")) {
+            $cookie = Cookie::forget("token");
+            return response()->json([
+                "message" => "The session was finished."
+            ], 200)->withCookie($cookie);
+        }
+        return response()->json([
+            "message" => "Error when finish the session"
+        ], 401);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,8 @@ use App\Http\Controllers\AuthController;
 
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/check', [AuthController::class, 'check']);
+Route::get('/check', [AuthController::class, 'checkToken']);
+Route::get('/destroy', [AuthController::class, 'destroyToken']);
 
 Route::middleware(['auth:api'])->group(function(){
     // Route::get('/rota-protegida', 'Controlador@metodo')->middleware('auth:api');
