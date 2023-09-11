1c2d66da7968e83001e7a16d7f7f59855456488d
FelipeSoft <felipedecastrolima@gmail.com>
2023-09-11 07:47:21 -0300

    fix: Otimizando rotas

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -38,27 +38,4 @@ class UserController extends Controller
             ]);
         }
     }
-
-    public function login(Request $request){
-        $this->validate($request, [
-            'email' => 'required|email',
-            'password' => 'required|min:8'
-        ]);
-
-        try{
-            $credentials = $request->only("email", "password");
-            if($token = JWTAuth::attempt($credentials)){
-                return response()->json([
-                    'token' => $token
-                ], 200);
-            }
-            return response()->json([
-                'error' => "Unauthorized"
-            ], 201);
-        } catch (QueryException $error) {
-            return response()->json([
-                'error' => 'Unauthorized'
-            ], 201);
-        }
-    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+Route::post('/login', [AuthenticationController::class, 'login']);
+Route::get('/destroy', [AuthenticationController::class, 'destroy']);
+Route::get('/check', [AuthenticationController::class, 'check']);
+
 Route::post('/register', [UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login']);
 
 Route::middleware(['auth:api'])->group(function(){
     // Route::get('/rota-protegida', 'Controlador@metodo')->middleware('auth:api');
