426cb91e82c6ad693b6d562c81fc3c321ec31580
desenvolvimento-th-solucoes <desenvolvimento@thsolucoes.com>
2023-09-28 11:55:47 -0300

    fix: Atualizando informações de configuração

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://odmzus.hospedagemelastica.com.br'),
 
     'asset_url' => env('ASSET_URL'),
 
