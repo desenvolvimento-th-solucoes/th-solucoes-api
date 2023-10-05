60d5af9d9feb45a132b62ad152abf13c77b9674b
desenvolvimento-th-solucoes <desenvolvimento@thsolucoes.com>
2023-10-05 10:38:12 -0300

    fix: Utilizando middleware de JWT API

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,10 @@ Route::middleware(['middleware' => 'jwt.api'])->group(function(){
     Route::get('/destroy', [AuthController::class, 'destroyToken']);
     Route::put('/save', [UserController::class, 'save']);
     Route::put('/redefine', [UserController::class, 'redefine']);
+    Route::get('/check', [AuthController::class, 'checkToken']);
 });
 
 Route::middleware(['throttle:5,1'])->group(function(){
-    Route::get('/check', [AuthController::class, 'checkToken']);
     Route::post('/contact', [MailController::class, 'contact']);
     Route::post('/register', [UserController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
