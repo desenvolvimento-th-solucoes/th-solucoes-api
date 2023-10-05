a6b167ea016f9c9321a05a95c7c917459ba4edab
desenvolvimento-th-solucoes <desenvolvimento@thsolucoes.com>
2023-10-05 11:25:51 -0300

    fix: Adicionando e-mail de destino

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -16,14 +16,15 @@ class MailController extends Controller
         ]);
 
         $contact_email = new ContactMailer($request->input("name"), $request->input("email"), $request->input("body"));
-        Mail::to("desenvolvimento@thsolucoes.com")->send($contact_email);    
+        Mail::to("ticket@thsolucoes.com")->send($contact_email);    
     }
+
     public function newsletter(Request $request){
         $this->validate($request, [
             "email" => "required|email",
         ]);
 
-        $contact_email = new ContactMailer("desenvolvimento@thsolucoes.com");
+        $contact_email = new ContactMailer("ticket@thsolucoes.com");
         Mail::to($request->input("email"))->send($contact_email);    
     }
 }
